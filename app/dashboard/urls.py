8e2416e5bb272bf0d76c3eb1ee8552a5b79ac459
Samuelbriones <samuelbriones558@gmail.com>
2025-11-16 10:31:45 -0500

    add: mejora de interfas

--- app/dashboard/urls.py
+++ app/dashboard/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_view, name='dashboard'),
+        path('', views.dashboard_view, name='dashboard'),
+        path('calendar/', views.calendar_view, name='dashboard_calendar'),
+        path('exams/', views.exams_view, name='dashboard_exams'),
+        path('materials/', views.materials_view, name='dashboard_materials'),
+        path('courses/', views.courses_view, name='dashboard_courses'),
 ]
--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -17,3 +17,23 @@ def dashboard_view(request):
 		'architect': arch,
 	}
 	return render(request, 'dashboard.html', context)
+
+@login_required
+def calendar_view(request):
+    """Renderiza la plantilla estática del calendario."""
+    return render(request, 'calendar.html')
+
+@login_required
+def exams_view(request):
+    """Renderiza la plantilla estática de exámenes."""
+    return render(request, 'exams.html')
+
+@login_required
+def materials_view(request):
+    """Renderiza la plantilla estática de materiales."""
+    return render(request, 'materials.html')
+
+@login_required
+def courses_view(request):
+    """Renderiza la plantilla estática de cursos."""
+    return render(request, 'courses.html')
